ce9ac317a3f11993ba5703ee8b7d4942a6eab3fb
Thomsen-79 <61097896+Thomsen-79@users.noreply.github.com>
2026-01-08 23:12:39 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'klinikpilgaard-v4';
+const CACHE_NAME = 'klinikpilgaard-v5';
 const BASE_PATH = '/klinikpilgaard-app';
 
 // Filer der altid skal være tilgængelige offline
@@ -59,6 +59,7 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
 
