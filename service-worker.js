ba518fecac3517ca8085c6784f0d43f1ae750099
Thomsen-79 <61097896+Thomsen-79@users.noreply.github.com>
2026-01-09 19:14:15 +0100

    Update service worker to version 7

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'klinikpilgaard-v6';
+const CACHE_NAME = 'klinikpilgaard-v7';
 const BASE_PATH = '/klinikpilgaard-app';
 
 // Filer der altid skal være tilgængelige offline
@@ -59,6 +59,7 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
 
